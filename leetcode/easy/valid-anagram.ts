0ea7b2b0c073984bcfad8b41c25a2838b4241bd2
Gearonix <egor.uzhanin@gmail.com>
2023-04-26 14:50:30 +0300

    valid-anagram

---
+++ leetcode/easy/valid-anagram.ts
@@ -0,0 +1,11 @@
+function isAnagram(s: string, t: string): boolean {
+  if (s.length !== t.length) return false
+  const goal = t.split('')
+  for (let str of s) {
+    if (goal.includes(str)) {
+      goal.splice(goal.indexOf(str), 1)
+    }
+  }
+  return !goal.length
+}
+
