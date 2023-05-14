05891e36d49cf22593c6dd5dff8fe33a8f9c47e3
Gearonix <egor.uzhanin@gmail.com>
2023-05-14 20:56:27 +0300

    generate-parentheses

---
+++ leetcode/medium/generate-parentheses/solution.ts
@@ -0,0 +1,16 @@
+const generateParenthesis = (limit: number, combo = '(',
+    opened = 1, closed = 0): string[] => {
+  if (opened === limit && closed === opened) {
+    return [combo]
+  }
+
+  const combos = []
+  if (opened < limit) {
+    combos.push(...generateParenthesis(limit, combo + '(', opened + 1, closed))
+  }
+  if (closed < opened) {
+    combos.push(...generateParenthesis(limit, combo + ')', opened, closed + 1))
+  }
+
+  return combos
+}
