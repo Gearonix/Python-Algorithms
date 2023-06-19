9f90e1d250a789dfc046eb88f6d6d05270bb17fd
Gearonix <egor.uzhanin@gmail.com>
2023-06-19 14:39:35 +0300

    search-a-2d-matrix

---
+++ leetcode/medium/search-a-2d-matrix/solution.ts
@@ -0,0 +1,57 @@
+function searchMatrix(matrix: number[][], target: number): boolean {
+  let left = 0
+  let right = matrix.length - 1
+
+  while (left <= right) {
+    const mid = getMiddle(left, right)
+    const first = matrix[mid][0]
+    const last = matrix[mid].at(-1) as number
+
+    if (target > last) {
+      left = mid + 1
+    }
+    if (target < first) {
+      right = mid - 1
+    }
+
+    if (target === first || target === last) {
+      return true
+    }
+
+    if (target > first && target < last) {
+      break
+    }
+  }
+
+  if (left > right) {
+    return false
+  }
+
+  const idx = getMiddle(left, right)
+
+  left = 0
+  right = matrix[idx].length
+
+  while (left <= right) {
+    const mid = getMiddle(left, right)
+    const guess = matrix[idx][mid]
+
+    if (target === guess) {
+      return true
+    }
+
+    if (target > guess) {
+      left = mid + 1
+    }
+    if (target < guess) {
+      right = mid - 1
+    }
+  }
+
+  return false
+}
+
+
+const getMiddle = (l: number, r: number) => {
+  return Math.floor((l + r) / 2)
+}
